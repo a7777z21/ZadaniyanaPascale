5986f3f95f46a4c79ea498bd20ba84246c9b08ab
a7777z21 <82153547+a7777z21@users.noreply.github.com>
2021-05-26 13:01:25 +0300

    Add files via upload

---
+++ 1 задание паскаль.pas
@@ -0,0 +1,10 @@
+﻿var A,i,c :integer;
+          begin 
+c:=0; 
+ for i:=1 to 15 do begin
+  write ('введите число: ');
+  readln(A);
+  if A<0 then c:=c+1;
+  end; 
+writeln ('количество отрицательных чисел = ', c);
+     end.
